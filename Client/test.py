8d221e61ab1036858bf8aa7ef288766bd9eb3e04
vybhavjain <vybhavjain@gmail.com>
2022-12-15 13:33:51 -0500

    Added some comments

--- Client/test.py
+++ Client/test.py
@@ -4,11 +4,12 @@ from unittest.mock import MagicMock
 from inventory_client import inventoryclient
 import get_book_titles
 
-
 class UnitTestMockServer(unittest.TestCase):
     
     def testsUnitTestMockServer(self):
         mock_client = inventoryclient()
+
+        #Mocking the return value of the function call
         mock_client.getBookTitles = MagicMock(return_value = ["Divergent","Insurgent"])
         mock_client.getBookTitles(["abcd-1000", "abcd-1001"], key = "value")
         
@@ -20,6 +21,8 @@ class UnitTestMockServer(unittest.TestCase):
 class UnitTestRealServer(unittest.TestCase):
     def testsUnitTestRealServer(self):
         real_client = inventoryclient()
+
+        #Running the real server to get the answer
         result = get_book_titles.getBookTitles(["abcd-1002"],real_client)
         expected_title = ["Allegiant"]
         self.assertEqual(result, expected_title)
